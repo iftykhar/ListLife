2e79bc8daa3c78e820e671bb00321b51bc4a974e
Iftykhar <s.m.ifty49@gmail.com>
2022-03-24 01:01:35 +0600

    updating table for user blade syntax

--- resources/views/theme/header.blade.php
+++ resources/views/theme/header.blade.php
@@ -1,9 +1,32 @@
+{{-- @extends(theme.header) --}}
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href={{asset('bootstrap/css/bootstrap.min.css')}}>
+    <link rel="stylesheet" href={{asset('bootstrap/css/bootstrap.min.css')}}> {{-- bootstrap 5 CSS used in the link tag  --}}
+    <link rel="stylesheet" href="{{asset('bootstrap/js/bootstrap.min.js')}}"> {{-- bootstrap 5 JS used in the link tag --}}
     <title>Users Document</title>
 </head>
+
+
+<body>
+  <div class="container">
+    <div class="header ">
+      <div class="col-4"></div>
+      <div class="col-4 " style="margin: 20px">
+        <p >Welcome User</p>
+      </div>
+      <div class="col-4"></div>
+    </div>
+    <div class="main-body">
+      <div class="col-1"></div>
+      <div class="col-10 justify-content-between">
+        @yield('content')
+      </div>
+      <div class="col-1"></div>
+    </div>
+  </div>
+</body>
+</html>
--- resources/views/user/user.blade.php
+++ resources/views/user/user.blade.php
@@ -1,49 +1,37 @@
-{{-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href={{asset('bootstrap/css/bootstrap.min.css')}}>
-    <title>Users Document</title>
-</head> --}}
 @extends('theme.header')
 
-<body>
-    <div class="container">
-        <div class="heading">
-            <div class="col-3"></div>
-            <div class="col-3" style="color: brown">
-                <strong><h1>Users Document</h1></strong>
-            </div>
-            <div class="col-3"></div>
-        </div>
-        <div class="main">
-            <div class="col-2"></div>
-            <div class="col-4">
-                <table class="table">
-                    <thead>
-                      ...
-                    </thead>
-                    <tbody>
-                      <tr class="table-active">
-                        ...
-                      </tr>
-                      <tr>
-                        ...
-                      </tr>
-                      <tr>
-                        <th scope="row">3</th>
-                        <td colspan="2" class="table-active">Larry the Bird</td>
-                        <td>@twitter</td>
-                      </tr>
-                    </tbody>
-                  </table>
-                  
-            </div>
-            <div class="col-2"></div>
-        </div>
-    </div>
-  
-</body>
-</html>
+@section('content')
+<h3>
+  Record list 
+</h3>
+<hr>
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Number</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+@endsection
